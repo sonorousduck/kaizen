afafa980c7ae4cd50bf03c54a340b7be517ae146
Ryan Anderson <sonorousduck@gmail.com>
2026-06-17 05:37:56 -0600

    Set up sentry and router

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,26 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import * as Sentry from '@sentry/vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './index.ts';
+import '@/styles/theme.less';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+Sentry.init({
+  dsn: 'https://28916b703ef147d4863e1184d765b264@logging.sonorousduck.com/3',
+  integrations: [
+    Sentry.browserTracingIntegration({ router }),
+    Sentry.vueIntegration({ app }),
+  ],
+  environment: import.meta.env.MODE,
+});
+
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+
+app.use(pinia);
+app.use(router);
+
+app.mount('#app');
