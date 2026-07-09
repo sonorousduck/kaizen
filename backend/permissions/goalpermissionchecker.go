5855a6fae6b0571f38578e36a4a302201eb52139
Ryan Anderson <sonorousduck@gmail.com>
2026-07-09 06:32:40 -0600

    Created permission checker that the user needs to own the goal to check it

---
+++ backend/permissions/goalpermissionchecker.go
@@ -0,0 +1,27 @@
+package permissions
+
+import (
+	"backend/models"
+	"backend/services"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
+
+type GoalPermissionChecker struct {
+	goalService *services.GoalService
+}
+
+func NewGoalPermissionChecker(goalService *services.GoalService) *GoalPermissionChecker {
+	return &GoalPermissionChecker{goalService: goalService}
+}
+
+func (goalPermissionChecker *GoalPermissionChecker) AuthUserHasPermissionOnGoal(ctx *gin.Context, authUser *models.User, goalId uuid.UUID) (bool, error) {
+	if authUser == nil {
+		return false, fmt.Errorf("error", "unauthorized")
+	}
+
+	return goalPermissionChecker.goalService.UserOwnsGoal(ctx, goalId, authUser.ID)
+
+}
