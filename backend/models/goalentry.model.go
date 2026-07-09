eff28a2f7458b2f8a1e8ae2c5aa9fc455aedeb71
Ryan Anderson <sonorousduck@gmail.com>
2026-07-09 06:15:35 -0600

    Created the db patch and model code for goal entries

--- backend/models/goalentry.model.go
+++ backend/models/goalentry.model.go
@@ -7,9 +7,23 @@ import (
 )
 
 type GoalEntry struct {
-	ID     uuid.UUID `json:"id" binding:"required"`
+	ID        uuid.UUID `json:"id" binding:"required"`
+	GoalId    uuid.UUID `json:"goalId" binding:"required"`
+	Value     float32   `json:"value" binding:"required"`
+	Note      *string   `json:"note"`
+	Date      time.Time `json:"date" binding:"required"`
+	CreatedAt time.Time `json:"createdAt" binding:"required"`
+}
+
+type CreateGoalEntry struct {
 	GoalId uuid.UUID `json:"goalId" binding:"required"`
 	Value  float32   `json:"value" binding:"required"`
 	Note   *string   `json:"note"`
 	Date   time.Time `json:"date" binding:"required"`
+}
+
+type UpdateGoalEntry struct {
+	Value float32   `json:"value" binding:"required"`
+	Note  *string   `json:"note"`
+	Date  time.Time `json:"date" binding:"required"`
 }
