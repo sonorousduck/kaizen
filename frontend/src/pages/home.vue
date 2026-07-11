37f82c3214df2f8c8e02a38380e07c15078aff29
Ryan Anderson <sonorousduck@gmail.com>
2026-07-11 15:00:13 -0600

    Converted the logger to use an enum instead

--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
+import type { ModelsGoal } from '@/generated/api';
+import { computedAsync } from '@vueuse/core';
 import { useRouter } from 'vue-router';
 import DuckButton from '@/components/core/DuckButton/DuckButton.vue';
+import { getGoalsByUser } from '@/generated/api';
 import { useAuthStore } from '@/stores/auth';
+import { logger } from '@/utils/logger';
+import { isSuccess } from '@/utils/response';
 
 const auth = useAuthStore();
 const router = useRouter();
+
+const goals = computedAsync(async () => {
+  const userGoals = await getGoalsByUser({});
+
+  if (!isSuccess<ReadonlyArray<ModelsGoal>>) {
+    logger.log('info');
+  }
+});
 
 async function logout() {
   const success = await auth.logout();
